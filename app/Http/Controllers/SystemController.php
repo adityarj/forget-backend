46472904d108d7938db7e648637aabac142cada3
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2016-12-12 21:22:02 +0800

    made changes to drawer container

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -11,7 +11,7 @@ class SystemController extends Controller
 
     }
 
-    //Below api is for when a negative weight is posted by the system
+    //Below api is for when a negative weight is posted by the system, update the table with negative weight
     public function getItemByWeight(Request $request) {
 
         $weight = abs($request->get('weight'));
@@ -20,22 +20,19 @@ class SystemController extends Controller
             ->where('weight','<=',$weight + 0.5)
             ->get();
 
-//        if ($item) {
-//            return json_encode($item);
-//        } else {
-//            return null;
-//        }
-
-        $data['status'] = 'success';
-        return json_encode($data);
+        if (!$item->isEmpty()) {
+            return json_encode($item);
+        } else {
+            $result['result'] = 'item not found';
+            return json_encode($result);
+        }
 
     }
 
-    //Below api is for when a  positive weight is posted by the system
+    //Below api is for when a  positive weight is posted by the system, update the required table with positive
     public function handleWeight(Request $request) {
 
         $item_original =  Item::where('weight',0)->get();
-
 
             $item = new Item();
             $item->weight = $request->get('weight');
@@ -43,20 +40,8 @@ class SystemController extends Controller
             $item->bin = 0;
             $item->save();
 
-
         $data['status'] = 'success';
         return json_encode($data);
 
-
-    }
-
-    public function transmitSignal() {
-
-        $appRequestSuccess = true;
-
-        if ($appRequestSuccess) {
-            $data['status'] = true;
-            return json_encode($data);
-        }
     }
 }
