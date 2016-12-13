67d2e95f9040b4da8b287f55a58479e8e5e0ce7a
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2016-12-14 07:08:16 +0800

    added some bullshit

--- app/Http/Controllers/analyticsConroller.php
+++ app/Http/Controllers/analyticsConroller.php
@@ -18,7 +18,7 @@ class analyticsConroller extends Controller
         $mintime = Carbon::parse($time);
         $mintime->subMinute();
 
-        $use = analyticsModel::where('date','<=',$maxtime)->where('date','>=',$mintime)->first();
+        $use = analyticsModel::where('date','<=',$maxtime)->orWhere('date','>=',$mintime)->first();
         if(!$use->isEmpty()) {
             $use->counter = $use->counter + 1;
             $use->save();
