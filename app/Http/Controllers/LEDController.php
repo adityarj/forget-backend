16e9b6c03015671e85b4ef4d514e2105066fc6b5
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2016-12-13 11:32:28 +0800

    added comments for LED controller

--- app/Http/Controllers/LEDController.php
+++ app/Http/Controllers/LEDController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class LEDController extends Controller
 {
+    //Set the LED as on/off for each compartment
     public function setLED(Request $request) {
         $led = activeLED::first();
         $led->light = $request->get('led');
@@ -16,6 +17,7 @@ class LEDController extends Controller
         return json_encode($result);
     }
 
+    //Get the current status of the LED
     public function getLEDStatus() {
         $led = activeLED::first();
         if ($led->light == 1) {
@@ -26,6 +28,7 @@ class LEDController extends Controller
         return json_encode($return);
     }
 
+    //Add a new LED entry, admin use only
     public function addLEDEntry() {
         $led = new activeLED();
         $led->bin = "comp1";
@@ -36,11 +39,13 @@ class LEDController extends Controller
         return json_encode($result);
     }
 
+    //Fetch all active LED controllers, admin use only
     public function fetchLEDEntry() {
         $led = activeLED::all();
         return json_encode($led);
     }
 
+    //Reset the LED roster
     public function reset() {
         activeLED::truncate();
 
