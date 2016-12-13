6bf991bed1adf357901f5977400a3445f42b2109
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2016-12-13 11:30:29 +0800

    added saving status

--- app/Http/Controllers/LEDController.php
+++ app/Http/Controllers/LEDController.php
@@ -10,6 +10,7 @@ class LEDController extends Controller
     public function setLED(Request $request) {
         $led = activeLED::first();
         $led->light = $request->get('led');
+        $led->save();
 
         $result['result'] = 'success';
         return json_encode($result);
