8379bc7748f0c275cb23aa15b9429191e763bf87
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-01-12 01:58:00 +0800

    removed api info

--- config/Google.php
+++ config/Google.php
@@ -5,11 +5,3 @@
  * Date: 27-11-2016
  * Time: 14:49
  */
-
-return [
-  'app_name' => '',
-  'client_id' => '101535231905175354247',
-  'client_secret' => '',
-  'api_key' => 'AIzaSyB73ntzecQe0kfNANDfiM2P_rFF68LT-fs',
-  'service_account_name' => 'forget-backend@appspot.gserviceaccount.com'
-];
